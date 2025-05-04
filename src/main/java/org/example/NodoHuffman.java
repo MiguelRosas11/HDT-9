2967462a3d823ee2036aac218501ce6a5521d4ae
MiguelRosas11 <roguemijose@gmail.com>
2025-05-04 02:23:53 -0600

    Clase NodoHuffman terminada

--- src/main/java/org/example/NodoHuffman.java
+++ src/main/java/org/example/NodoHuffman.java
@@ -1,4 +1,29 @@
 package org.example;
 
-public class NodoHuffman {
+public class NodoHuffman implements Comparable<NodoHuffman> {
+    public char caracter;
+    public int frecuencia;
+    public NodoHuffman izquierdo;
+    public NodoHuffman derecho;
+
+    public NodoHuffman(char caracter, int frecuencia) {
+        this.caracter = caracter;
+        this.frecuencia = frecuencia;
+    }
+
+    public NodoHuffman(int frecuencia, NodoHuffman izquierdo, NodoHuffman derecho) {
+        this.caracter = '\0'; // nodo interno (no es hoja)
+        this.frecuencia = frecuencia;
+        this.izquierdo = izquierdo;
+        this.derecho = derecho;
+    }
+
+    public boolean esHoja() {
+        return izquierdo == null && derecho == null;
+    }
+
+    @Override
+    public int compareTo(NodoHuffman otro) {
+        return Integer.compare(this.frecuencia, otro.frecuencia);
+    }
 }
